a9f9bec415723fe5f076942d057b4508e7c14bb1
SZILAS Benjamin  / EVVA Wien <b.szilas@evva.com>
2025-11-09 12:45:28 +0100

    FEAT determine threads from arguments

--- Task3/prime_time.py
+++ Task3/prime_time.py
@@ -2,13 +2,35 @@
 
 import time
 import threading
+import argparse
+import multiprocessing
+from sys import argv
+
 from is_prime import is_prime
 from random_list import random_list
 
-THREADS = 2
-
 result = 0
 lock = threading.Lock()
+
+def parse_arguments() -> argparse.Namespace:
+    parser = argparse.ArgumentParser(
+        description="prime counter options",
+        formatter_class=argparse.RawDescriptionHelpFormatter,
+        epilog=f"""
+Examples:
+  {argv[0]} --multi-threaded
+  {argv[0]} --mt
+        """
+    )
+
+    parser.add_argument(
+        '--multi-threaded', '--mt',
+        action='store_true',
+        help=f'use multiple threads '
+    )
+
+    return parser.parse_args()
+
 
 def count_primes(A: list[int]) -> int:
     prime_counter = 0
@@ -25,13 +47,13 @@ def add_prime_count_to_global(A: list[int]):
         result += local_count
 
 
-def count_primes_threaded() -> int:
+def count_primes_threaded(thread_count: int) -> int:
     global result
     full_list = random_list()
-    part_length = len(full_list) / THREADS
+    part_length = len(full_list) / thread_count
     threads = []
 
-    for i in range(THREADS):
+    for i in range(thread_count):
         start = int(i * part_length)
         end = int((i + 1) * part_length)
         partial_list = full_list[start:end]
@@ -39,20 +61,21 @@ def count_primes_threaded() -> int:
         threads.append(t)
         threads[i].start()
 
-    for i in range(THREADS):
+    for i in range(thread_count):
         threads[i].join()
 
     return result
 
 
 def main():
+    arguments = parse_arguments()
+    cpu_count = multiprocessing.cpu_count()
     start = time.time()
-    #######################################
-    # SINGLE THREADED IMPLEMENTATION
-    # count = count_primes(random_list())
-    #######################################
-    # MULTI THREADED
-    count = count_primes_threaded()
+    if arguments.multi_threaded:
+        print(f"threads: {cpu_count}")
+        count = count_primes_threaded(cpu_count)
+    else:
+        count = count_primes(random_list())
     end = time.time()
     print(f"counted {count} primes in {end - start} seconds")
 
